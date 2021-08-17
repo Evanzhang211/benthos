137dc1d8e178e306a3ea53252536d17a28b2cb74
418072832@qq.com <418072832@qq.com>
2021-08-17 23:25:24 +0800

    plugin init

--- cmd/plugin/plugin.go
+++ cmd/plugin/plugin.go
@@ -1,1 +1,16 @@
-package plugin
+package main
+
+import (
+	"context"
+	"github.com/Jeffail/benthos/v3/public/service"
+
+	// Add your plugin packages here
+	_ "github.com/benthosdev/benthos-plugin-example/bloblang"
+	_ "github.com/benthosdev/benthos-plugin-example/input"
+	_ "github.com/benthosdev/benthos-plugin-example/output"
+	_ "github.com/benthosdev/benthos-plugin-example/processor"
+)
+
+func main() {
+	service.RunCLI(context.Background())
+}
